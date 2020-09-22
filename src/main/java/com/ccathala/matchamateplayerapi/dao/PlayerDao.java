64880a9b3493f4ba4b1b8758a519da1c3e7d14fa
ccathala <cathala.cha@gmail.com>
2020-09-22 12:10:45 +0200

    Minor changes

--- src/main/java/com/ccathala/matchamateplayerapi/dao/PlayerDao.java
+++ src/main/java/com/ccathala/matchamateplayerapi/dao/PlayerDao.java
@@ -1,5 +1,7 @@
 package com.ccathala.matchamateplayerapi.dao;
 
+import java.util.Optional;
+
 import com.ccathala.matchamateplayerapi.models.Player;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -7,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "players", path = "players")
-public interface PlayerDao extends MongoRepository<Player, String>{    
+public interface PlayerDao extends MongoRepository<Player, String>{  
+    Optional<Player> findByEmail(@Param("email") String email);
     Player deleteByEmail(@Param("email") String email);
 }
